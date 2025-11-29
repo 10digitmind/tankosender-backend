1c5e453d79ff56b558f12a8a67843ea7507c5e9c
Your Name <olubodekehinde2019@gmail.com>
2025-11-29 13:51:49 +0000

    sender updated

--- Nodemailer/nodemailer.js
+++ Nodemailer/nodemailer.js
@@ -1,34 +1,28 @@
+// transporter.js
 const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
 
-async function createTransporter() {
-  const hbs = await import('nodemailer-express-handlebars'); // dynamic import
+const transporter = nodemailer.createTransport({
+  host: process.env.BREVO_HOST,
+  port: process.env.BREVO_PORT,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
+  }
+});
 
-  const transporter = nodemailer.createTransport({
-    host:  process.env.BREVO_HOST,
-    port: process.env.BREVO_PORT,
-    secure: false,
-      requireTLS: true, 
-    auth: {
-      user: process.env.BREVO_USER,
-      pass: process.env.BREVO_PASS
-    }
-  });
+const viewsPath = path.join(__dirname, '..', 'Views');
 
+transporter.use(
+  'compile',
+  hbs({
+    viewEngine: { partialsDir: viewsPath, defaultLayout: false },
+    viewPath: viewsPath,
+    extName: '.hbs'
+  })
+);
 
-  transporter.use(
-    'compile',
-    hbs.default({ // note the `.default` when importing ESM
-      viewEngine: {
-        partialsDir: path.resolve('./Views/'),
-        defaultLayout: false
-      },
-      viewPath: path.resolve('./Views/'),
-      extName: '.hbs'
-    })
-  );
-
-  return transporter;
-}
-
-module.exports = createTransporter;
+module.exports = transporter;
